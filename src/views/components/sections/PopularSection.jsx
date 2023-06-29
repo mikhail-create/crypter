a37d7e7d580d1c73dd07f4555727733f83023574
Mikhail <leonovm143@gmail.com>
2023-06-29 16:36:26 +0600

    some fixes

--- src/views/components/sections/PopularSection.jsx
+++ src/views/components/sections/PopularSection.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { ReactComponent as ArrowDown } from '../../../assets/icons/arrow-down.svg'
+import { ReactComponent as ArrowLeft } from '../../../assets/icons/arrow-left.svg'
+import { ReactComponent as ArrowRight } from '../../../assets/icons/arrow-right.svg'
 import SliderCard from '../SliderCard'
 
 function PopularSection() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const slides = [
+        <SliderCard rank={1} name='Edd Harris' wallet={2.456} />,
+        <SliderCard rank={2} name='Edd Harris' wallet={2.456} />,
+        <SliderCard rank={3} name='Edd Harris' wallet={2.456} />,
+        <SliderCard rank={4} name='Edd Harris' wallet={2.456} />,
+        <SliderCard rank={5} name='Edd Harris' wallet={2.456} />,
+        <SliderCard rank={6} name='Edd Harris' wallet={2.456} />,
+        <SliderCard rank={7} name='Edd Harris' wallet={2.456} />,
+        <SliderCard rank={8} name='Edd Harris' wallet={2.456} />,
+        <SliderCard rank={9} name='Edd Harris' wallet={2.456} />,
+        <SliderCard rank={10} name='Edd Harris' wallet={2.456} />,
+    ]
+
+    const handlePrevSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === 0 ? slides.length - 5 : prevIndex - 1));
+    };
+
+    const handleNextSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === slides.length - 5 ? 0 : prevIndex + 1));
+    };
+
+    const visibleSlides = slides.slice(currentIndex, currentIndex + 5);
+
     return (
         <section className='popular'>
             <div className='popular-header'>
@@ -27,11 +54,22 @@ function PopularSection() {
                 </div>
             </div>
             <div className='popular-slider'>
-                <SliderCard rank={1} name='Edd Harris' wallet={2.456}/>
-                <SliderCard rank={2} name='Edd Harris' wallet={2.456}/>
-                <SliderCard rank={3} name='Edd Harris' wallet={2.456}/>
-                <SliderCard rank={4} name='Edd Harris' wallet={2.456}/>
-                <SliderCard rank={5} name='Edd Harris' wallet={2.456}/>
+                <button className='popular-slider--control' onClick={handlePrevSlide}>
+                    <ArrowLeft />
+                </button>
+                <div id='slider-list' className='popular-slider--list'>
+                    {visibleSlides.map((slide, index) => (
+                        <div
+                            key={index}
+                            className={`slider--item ${index === 0 ? 'active' : ''}`}
+                        >
+                            {slide}
+                        </div>
+                    ))}
+                </div>
+                <button className='popular-slider--control' onClick={handleNextSlide}>
+                    <ArrowRight />
+                </button>
             </div>
         </section>
     )
