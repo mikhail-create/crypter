cccf7e1320573e3ab22e5a4e6f964e6464e01945
Mikhail <leonovm143@gmail.com>
2023-07-03 15:25:01 +0600

    Medium card hover effect done

--- src/views/components/CardMedium.jsx
+++ src/views/components/CardMedium.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from '../components/Button'
 import NftImage from '../../assets/NFT-1.png'
 import AvatarImage from '../../assets/avatar.png'
 import AvatarImage01 from '../../assets/avatar-01.png'
 import AvatarImage02 from '../../assets/avatar-02.png'
 import { ReactComponent as CandlesticksUp } from '../../assets/icons/candlesticks-up.svg'
+import { ReactComponent as Heart } from '../../assets/icons/heart.svg'
 
 function CardMedium({ title, instaPrice, stockAmount, bid, isNewBids }) {
+    const [isHovered, setIsHovered] = useState(false)
+
+    const handleMouseEnter = () => {
+        setIsHovered(true)
+    }
+
+    const handleMouseLeave = () => {
+        setIsHovered(false)
+    }
+
     return (
-        <div className='card-medium'>
+        <div className='card-medium'
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
+
+            {
+                isHovered &&
+                <div className='card-hover' >
+                    <div className='card-hover--header'>
+                        <span className='card-hover--indicator'>
+                            purchasing !
+                        </span>
+                        <button className='card-hover--like'>
+                            <Heart />
+                        </button>
+                    </div>
+                    <div className='card-hover--button'>
+                        <Button
+                            text='Place a bid'
+                            isColored={true}
+                            buttonWidth={134}
+                            buttonHeight={40}
+                        />
+                    </div>
+                </div>
+            }
             <img srcSet={NftImage} alt={title + ' Nft Image'} />
             <div className='card-medium-description'>
                 <div className='stats'>
