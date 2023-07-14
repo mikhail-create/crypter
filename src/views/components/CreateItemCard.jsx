843fc5e5c4eeedc12b5e43f733208c77bf0195ab
Mikhail <leonovm143@gmail.com>
2023-07-14 10:57:56 +0600

    🚧 Changed Upload Page for mobile view

--- src/views/components/CreateItemCard.jsx
+++ src/views/components/CreateItemCard.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Button from 'views/components/shared/Button'
-import SingleItemImage from 'assets/create--single.png'
-import MultipleItemImage from 'assets/create--multiple.png'
+import SingleItemImage from 'assets/create-single.png'
+import SingleItemImageMini from 'assets/create-single--mini.png'
+import MultipleItemImage from 'assets/create-multiple.png'
+import MultipleItemImageMini from 'assets/create-multiple--mini.png'
+import { ReactComponent as ArrowRight } from 'assets/icons/arrow-right.svg'
 
 function CreateItemCard({ isSingle }) {
   return (
@@ -9,14 +12,28 @@ function CreateItemCard({ isSingle }) {
       {isSingle
         ?
         <>
+          <img srcSet={SingleItemImageMini} alt='Create Single Item' />
           <img srcSet={SingleItemImage} alt='Create Single Item' />
+          <label htmlFor='createButton' className='font-body--2-bold'>
+            Create Multiple
+          </label>
+          <button id='createButton'>
+            <ArrowRight fill='#777E90' />
+          </button>
           <div className='create-item--button'>
             <Button text='Create Single' path='/upload/single' />
           </div>
         </>
         :
         <>
+          <img srcSet={MultipleItemImageMini} alt='Create Multiple Item' />
           <img srcSet={MultipleItemImage} alt='Create Multiple Item' />
+          <label htmlFor='createButton' className='font-body--2-bold'>
+            Create Multiple
+          </label>
+          <button id='createButton'>
+            <ArrowRight fill='#777E90' />
+          </button>
           <div className='create-item--button'>
             <Button text='Create Multiple' path='/upload/multiple' isColored={true} />
           </div>
--- src/views/pages/UploadPage.jsx
+++ src/views/pages/UploadPage.jsx
@@ -1,58 +1,52 @@
-// import { React, useEffect } from 'react'
-// import Button from 'views/components/shared/Button'
-// import { ReactComponent as ArrowLeft } from 'assets/icons/arrow-left.svg';
-// import { ReactComponent as ArrowRight } from 'assets/icons/arrow-right.svg';
-// import CreateItemCard from 'views/components/CreateItemCard';
-
-// function UploadPage() {
-//   useEffect(() => {
-//     document.title = 'Upload'
-//   }, [])
-
-//   return (
-//     <div className='upload'>
-//       <div className='upload-nav'>
-//         <Button text='Back to home' iconPosition='Left' icon={<ArrowLeft />} path='/' />
-//         <div className='upload-nav--path'>
-//           <span className='font-button--2'>
-//                         Home
-//           </span>
-//           <ArrowRight />
-//           <span className='font-button--2'>
-//                         Upload Item
-//           </span>
-//         </div>
-//       </div>
-//       <div className='upload--divider'></div>
-//       <div className='upload-headline'>
-//         <h2 className='font-headline--2'>
-//                     Upload item
-//         </h2>
-//         <p className='font-caption--1 upload-headline--description'>
-//                     Choose <span>“Single”</span> if you want your collectible to be one of a kind or <span>“Multiple”</span> if you want to sell 
-//                     one collectible multiple times
-//         </p>
-//       </div>
-//       <div className='upload-cards'>
-//         <div className='upload-cards--option'>
-//           <CreateItemCard isSingle={true} />
-//           <CreateItemCard />
-//         </div>
-//         <span className='font-caption--2'>
-//                     We do not own your private keys and cannot access your funds without your confirmation.
-//         </span>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default UploadPage
-
-import React from 'react'
+import { React, useEffect } from 'react'
+import Button from 'views/components/shared/Button'
+import { ReactComponent as ArrowLeft } from 'assets/icons/arrow-left.svg';
+import { ReactComponent as ArrowRight } from 'assets/icons/arrow-right.svg';
+import CreateItemCard from 'views/components/CreateItemCard';
 
 function UploadPage() {
+  useEffect(() => {
+    document.title = 'Upload'
+  }, [])
+
   return (
-    <div>UploadPage</div>
+    <div className='upload'>
+      <div className='upload-nav'>
+        <Button text='Back to home' iconPosition='Left' icon={<ArrowLeft />} path='/' />
+        <div className='upload-nav--path'>
+          <span className='font-button--2'>
+            Home
+          </span>
+          <ArrowRight />
+          <span className='font-button--2'>
+            Upload Item
+          </span>
+        </div>
+      </div>
+      <div className='upload--divider'></div>
+      <div className='upload-headline'>
+        <h2 className='font-headline--2'>
+          Upload item
+        </h2>
+        <p className='font-caption--1 upload-headline--description'>
+          Choose <span>“Single”</span> if you want your collectible to be one of a kind or <span>“Multiple”</span> if you want to sell one
+          collectible multiple times
+        </p>
+      </div>
+      <div className='upload-cards'>
+        <div className='upload-cards--option'>
+          <div>
+            <CreateItemCard isSingle={true} />
+          </div>
+          <div>
+            <CreateItemCard />
+          </div>
+        </div>
+        <span className='font-caption--2 upload-cards--description'>
+          We do not own your private keys and cannot access your funds without your confirmation.
+        </span>
+      </div>
+    </div>
   )
 }
 
