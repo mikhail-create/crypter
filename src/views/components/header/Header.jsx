d807f24c1246f9ebe4c17e77b00e878608b83725
Mikhail <leonovm143@gmail.com>
2023-07-14 10:18:02 +0600

    🚧 Created Header mobile view

--- src/views/components/header/Header.jsx
+++ src/views/components/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Logo from 'assets/logo_horizontal.png'
+import Avatar from 'assets/avatar.png'
+import { ReactComponent as Menu } from 'assets/icons/menu.svg'
 import SearchBar from './SearchBar'
 import Notification from './Notification'
 import Button from 'views/components/shared/Button'
@@ -11,21 +13,31 @@ function Header() {
         <div className="layout-left">
           <div className="left-logo">
             <div className="left-logo--image">
-              <img srcSet={Logo} alt='Logo'/>
+              <img srcSet={Logo} alt='Logo' />
             </div>
           </div>
           <div className="left--divider" />
           <div className="left--link font-button font-button--2">
-                        Discover
+            Discover
           </div>
           <div className="left--link font-button font-button--2">
-                        How it work
+            How it work
           </div>
         </div>
         <div className="layout-right">
-          <SearchBar />
-          <Notification />
-          <div className="right--buttons">
+          <div className='layout-right--search'>
+            <SearchBar />
+          </div>
+          <div className='layout-right--notification'>
+            <Notification />
+          </div>
+          <div className='layout-right--avatar'>
+            <img srcSet={Avatar} alt='Avatar Image' />
+          </div>
+          <div className='layout-right--menu'>
+            <Menu />
+          </div>
+          <div className="layout-right--buttons">
             <Button path='upload' text='Upload' isColored={true} />
             <Button path='connect' text='Connect Wallet' />
           </div>
