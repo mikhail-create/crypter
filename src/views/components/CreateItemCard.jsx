5a87df109bd454742b9afd96f74f85043dcf25c6
Mikhail <leonovm143@gmail.com>
2023-07-14 11:01:47 +0600

    🐛 routes fix

--- src/views/components/CreateItemCard.jsx
+++ src/views/components/CreateItemCard.jsx
@@ -5,6 +5,7 @@ import SingleItemImageMini from 'assets/create-single--mini.png'
 import MultipleItemImage from 'assets/create-multiple.png'
 import MultipleItemImageMini from 'assets/create-multiple--mini.png'
 import { ReactComponent as ArrowRight } from 'assets/icons/arrow-right.svg'
+import { Link } from 'react-router-dom'
 
 function CreateItemCard({ isSingle }) {
   return (
@@ -14,9 +15,9 @@ function CreateItemCard({ isSingle }) {
         <>
           <img srcSet={SingleItemImageMini} alt='Create Single Item' />
           <img srcSet={SingleItemImage} alt='Create Single Item' />
-          <label htmlFor='createButton' className='font-body--2-bold'>
+          <Link to={'/upload/single'} htmlFor='createButton' className='font-body--2-bold'>
             Create Multiple
-          </label>
+          </Link>
           <button id='createButton'>
             <ArrowRight fill='#777E90' />
           </button>
@@ -28,9 +29,9 @@ function CreateItemCard({ isSingle }) {
         <>
           <img srcSet={MultipleItemImageMini} alt='Create Multiple Item' />
           <img srcSet={MultipleItemImage} alt='Create Multiple Item' />
-          <label htmlFor='createButton' className='font-body--2-bold'>
+          <Link to='/upload/multiple' htmlFor='createButton' className='font-body--2-bold'>
             Create Multiple
-          </label>
+          </Link>
           <button id='createButton'>
             <ArrowRight fill='#777E90' />
           </button>
